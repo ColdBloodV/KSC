911279f72f7f360b95214ffadb028c77bbda1d3a
France <93790565+ColdBloodV@users.noreply.github.com>
2026-07-18 17:41:45 -0400

    Add serial communication script for Pico control
    This script communicates with a Pico device over serial, listens for commands, and shuts down the system upon receiving a 'SHUTOFF' command.

---
+++ KAOS-2/Test_Modules/2Wscript.py
@@ -0,0 +1,41 @@
+import serial
+import time
+import os
+
+pico = serial.Serial(
+    "/dev/serial0",
+    115200,
+    timeout=1
+)
+
+time.sleep(2)
+
+print("KAOS Pi controller started")
+
+while True:
+
+    if pico.in_waiting:
+
+        command = pico.readline().decode().strip()
+
+        print("Pico says:", command)
+
+        if command == "SHUTOFF":
+
+            print("Shutdown command received")
+
+            # Stop your camera/video 
+
+            print("Camera stopped")
+
+            # Tell Pico that the Pi is ready to lose power
+            pico.write(b"DONE\n")
+
+            print("DONE sent to Pico")
+
+            # Shut down Linux
+            os.system("sudo shutdown -h now")
+
+            break
+
+    time.sleep(0.1)
